f9aa9ef138eef595aae0205cab34ddf43b5a3685
Davlee1 <davidlee21019@gmail.com>
2025-12-30 12:31:45 -0500

    more fixes

--- 02-express-tutorial/controllers/people.js
+++ 02-express-tutorial/controllers/people.js
@@ -2,7 +2,7 @@ let { people } = require("../data.js");
 
 const addPerson = (req, res) => {
   if (!req.body.name) {
-    res.status(400).json({ success: false, message: "Please provide a name" });
+    return res.status(400).json({ success: false, message: "Please provide a name" });
   } else {
     people.push({ id: people.length + 1, name: req.body.name });
     res.status(201).json({ success: true, name: req.body.name });
@@ -27,12 +27,36 @@ const deletePerson = (req, res) => {
   const idToFind = parseInt(req.params.personID);
   const person = people.find((p) => p.id === idToFind);
   if (!person) {
-    return res.status(404).json({ success: false, message: `No person with id ${idToFind}` });
+    return res
+      .status(404)
+      .json({ success: false, message: `No person with id ${idToFind}` });
   } else {
-
     let people = people.filter((p) => p.id !== idToFind);
     res.status(200).json({ success: true, data: people });
   }
 };
 
-module.exports = { addPerson, getPeople, getPerson, deletePerson };
+const updatePerson = (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json({ success: false, message: "Please provide a name" });
+  }
+  const idToFind = parseInt(req.params.personID);
+  const person = people.find((p) => p.id === idToFind);
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, message: `No person with id ${idToFind}` });
+  } else {
+    people[idToFind] = req.body.name;
+    res.status(200).json({ success: true, data: people });
+  }
+};
+
+module.exports = {
+  addPerson,
+  getPeople,
+  getPerson,
+  deletePerson,
+  updatePerson,
+};
--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router  = express.Router();
-const { addPerson, getPeople, getPerson, deletePerson } = require("../controllers/people.js");
+const { addPerson, getPeople, getPerson, deletePerson, updatePerson} = require("../controllers/people.js");
 
 router.get("/", getPeople);
 
 router.get("/:personID", getPerson);
+router.put("/:personID", updatePerson);
 router.delete("/:personID", deletePerson);
 
 router.post("/", addPerson);
