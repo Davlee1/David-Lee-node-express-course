b5c06fe4d76a2b81910e9ba9dbc629c6eb0bf0cf
Davlee1 <davidlee21019@gmail.com>
2026-01-03 20:30:52 -0500

    week 5 progress

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,37 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks.js");
+const connectDB = require("./db/connect");
+const { loadEnvFile } = require("node:process");
+// Loads environment variables from the default .env file
+require("dotenv");
+
+//middlewarwe
+app.use(express.static("./public"));
+app.use(express.json());
+
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+//app.get('api/v1/tasks')
+//app.post('api/v1/tasks')
+//app.get('api/v1/tasks/:id')
+//app.patch('api/v1/tasks/:id')
+//app.delete('api/v1/tasks/:id')
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.CONNECT);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
