ca0b93b215773cb6ef4ceaa2b745331754a7f03f
Davlee1 <davidlee21019@gmail.com>
2026-01-09 17:00:08 -0500

    bug fixes

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 //routes
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
-
+app.use(errorHandlerMiddleware);
 
 const port = 3000;
 
--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -12,7 +12,7 @@ const createTask = asyncWrapper(async (req, res) => {
   res.status(201).json({ task });
 });
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
@@ -21,7 +21,7 @@ const getTask = asyncWrapper(async (req, res) => {
   res.status(200).json({ task });
 });
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
 
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
@@ -36,7 +36,7 @@ const updateTask = asyncWrapper(async (req, res) => {
   res.status(200).json({ task });
 });
 
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
