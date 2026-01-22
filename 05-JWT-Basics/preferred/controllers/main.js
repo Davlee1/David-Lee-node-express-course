95a7a99d0c81e03d9cdf1ea8db9a667de3cdffaf
Davlee1 <davidlee21019@gmail.com>
2026-01-21 23:59:04 -0500

    week 8 completion

---
+++ 05-JWT-Basics/preferred/controllers/main.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const logon = async (req, res) => {
+  const { name, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(200)
+      .json({ message: "Please provide email and password" });
+  }
+  const token = jwt.sign({ name, password }, process.env.SECRET, {
+    expiresIn: "24h",
+  });
+  res.status(200).json({ token: token });
+};
+
+const hello = async (req, res) => {
+  res.status(200).json({
+    msg: `Hello ${req.user.name}! Welcome!`,
+  });
+};
+
+module.exports = {
+  logon,
+  hello,
+};
